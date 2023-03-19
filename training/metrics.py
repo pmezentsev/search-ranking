11d150d81a47fd53153dfade2ada4ac3eb80b299
Pavel Mezentsev <pmezentsev@pulsepoint.com>
2023-03-19 12:48:50 -0400

    Clarifies notebook and moves complex metrics evaluation into one of the minor modules

--- training/metrics.py
+++ training/metrics.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+import pandas as pd
+import torch
 
 def dcg_k(ys_true: np.array, ys_pred: np.array, ndcg_top_k) -> float:
     indexes = np.argsort(-ys_pred)
@@ -17,3 +18,16 @@ def ndcg_k(ys_true: np.array, ys_pred: np.array, ndcg_top_k: int = 10) -> float:
     if ideal_dcg_score == 0 or np.isnan(ideal_dcg_score) or np.isnan(dcg_score):
         return 0
     return dcg_score / ideal_dcg_score
+
+
+def evaluate(model: torch.nn.Module = None, data: torch.utils.data.DataLoader = None) -> float:
+    labels_and_groups = data.dataset.samples_list
+    labels_and_groups = pd.DataFrame(labels_and_groups, columns=['left_id', 'right_id', 'rel'])
+
+    pred = [model.predict(batch).detach().numpy() for batch, _ in data]
+    pred = np.concatenate(pred, axis=0)
+    labels_and_groups['pred'] = pred
+
+    ndcg_list = [ndcg_k(df.rel.values, df.pred.values) for _, df in labels_and_groups.groupby('left_id')]
+    mean_ndcg = float(np.mean(ndcg_list))
+    return mean_ndcg
